7b9770a9f44d0c31ea5038e1bd8f63f8169d8237
dpajek <dan.pajek@gmail.com>
2020-09-17 23:39:38 -0400

    Got board UI done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
+/* eslint-disable class-methods-use-this */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <center>
+      <div className="App">
+        <StatusDisplay />
+        <Board />
+      </div>
+    </center>
   );
+}
+
+function StatusDisplay() {
+  return <div className="status">The status message</div>;
+}
+
+function Disc(props) {
+  const { colour } = props;
+
+  let disc = <div className="disc-white" />;
+
+  if (colour === 'Y')
+    disc = (
+      <div className="disc-border-Y">
+        <div className="disc-middle-Y" />
+      </div>
+    );
+  else if (colour === 'R')
+    disc = (
+      <div className="disc-border-R">
+        <div className="disc-middle-R" />
+      </div>
+    );
+  else disc = <div className="disc-white" />;
+
+  return <div className="disc-blue">{disc}</div>;
+}
+
+class Board extends React.Component {
+  renderDisc(colour) {
+    return <Disc colour={colour} />;
+  }
+
+  render() {
+    const R = 'R';
+    const Y = 'Y';
+    return (
+      <div>
+        <div className="board">
+          {this.renderDisc()}
+          {this.renderDisc(R)}
+          {this.renderDisc(R)}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc(Y)}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc(Y)}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc(R)}
+          {this.renderDisc(R)}
+          {this.renderDisc(R)}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+          {this.renderDisc()}
+        </div>
+        <br />
+        <button type="button" className="reset-button">
+          Reset
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
