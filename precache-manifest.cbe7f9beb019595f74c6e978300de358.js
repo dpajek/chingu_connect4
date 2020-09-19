a3981b9eabfe3587aef57b873fcab8327f978b27
dpajek <dan.pajek@gmail.com>
2020-09-18 21:30:42 -0400

    Updates

---
+++ precache-manifest.cbe7f9beb019595f74c6e978300de358.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f856a6828a881d85ec5b6ba4a68431d",
+    "url": "/chingu_connect4/index.html"
+  },
+  {
+    "revision": "6f56ce596829bd6d248c",
+    "url": "/chingu_connect4/static/css/main.0b6abd0f.chunk.css"
+  },
+  {
+    "revision": "37a2cb13a430389658aa",
+    "url": "/chingu_connect4/static/js/2.ac1b2116.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chingu_connect4/static/js/2.ac1b2116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f56ce596829bd6d248c",
+    "url": "/chingu_connect4/static/js/main.bf144697.chunk.js"
+  },
+  {
+    "revision": "afb242c31efe5f4db2d4",
+    "url": "/chingu_connect4/static/js/runtime-main.87ae93bd.js"
+  }
+]);
